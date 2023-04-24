1fee88d512424c22c1f67e596f99247c3993f06a
Caique <caiquemorales20@gmail.com>
2023-04-24 16:41:20 -0300

    :tada: First commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+
+// Functional Component
+const App = () => {
+  // Rendering
+  return (
+    <View>
+      <Text>Controle de finanças</Text>
+    </View>
+  );
+};
+
+// Export
+export default App;
